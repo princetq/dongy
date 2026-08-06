9f8b1576bab5e74e9bce2523ba3c0876d587d41d
princetq <princetq@gmail.com>
2026-08-06 11:51:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='phat-thuoc-dong-y-v1-10-github';
+const CACHE_NAME='phat-thuoc-dong-y-v1-11-github';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
